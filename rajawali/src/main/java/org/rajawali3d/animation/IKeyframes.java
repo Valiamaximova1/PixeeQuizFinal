674246660e23e9da152532344e323861b9f52bb1
Valentina Maximova <44725726+Valiamaximova1@users.noreply.github.com>
2025-04-15 09:41:03 +0300

    Full project reset to current local state

--- rajawali/src/main/java/org/rajawali3d/animation/IKeyframes.java
+++
@@ -1,7 +0,0 @@
-package org.rajawali3d.animation;
-
-public interface IKeyframes<K, V> {
-
-        V calculatePoint(K factor);
-
-}
--- rajawali/src/main/java/org/rajawali3d/curves/ICurve1D.java
+++
@@ -1,8 +0,0 @@
-package org.rajawali3d.curves;
-
-
-public interface ICurve1D {
-
-    int calculatePoint(double t);
-
-}
